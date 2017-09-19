05bf1242e700bbd28497c237940a93583d995d9d
Desmond Schmidt <schmidda@cl2n058.ib0.hpc.qut.edu.au>
2017-09-19 12:57:05 +1000

    Added vmd

--- ebfiles/v/vmd/make-patches.sh
+++ ebfiles/v/vmd/make-patches.sh
@@ -2,4 +2,4 @@
 # for new versions of vmd extract the perl configure script,
 # replace the default values of $install_bin_dir and $install_library_dir
 # with references to environment variables. These are set in the easyconfig.
-diff -Naur configure.orig configure > vmd-configure.patch
+diff -Naur 1.9.3/configure.orig 1.9.3/configure > vmd-1.9.3-configure.patch
