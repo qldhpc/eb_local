71801a8c100b424fa4343405110c0f24661dd92f
Desmond Schmidt <schmidda@cl2n058.ib0.hpc.qut.edu.au>
2017-09-21 17:16:40 +1000

    Updated tetgen

--- ebfiles/t/tetgen/make-patch.sh
+++ ebfiles/t/tetgen/make-patch.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-sourcepath=`eb --show-config | grep "sourcepath" | awk '{print $4}'`
+prefix=`eb --show-config | grep "prefix" | awk '{print $4}'`
 read -p "version (1.5.0):" input
 version=${input:-"1.5.0"}
 read -p "toolchain (foss-2017a):" input
 toolchain=${input:-"foss-2017a"}
+fullpath=$prefix/software/tetgen/$version-$toolchain
 cp makefile-raw new/makefile
 cat <<InstallTarget >> new/makefile
-install:
-        mkdir -p $sourcepath/tetgen/$version-$toolchain/bin
-        mkdir -p $sourcepath/tetgen/$version-$toolchain/lib
-        cp libtet.a $sourcepath/tetgen/$version-$toolchain/lib
-        cp tetgen /pkg/suse12/software/tetgen/$version-$toolchain/bin
+
+.PHONY: install
+install: 
+	mkdir -p $fullpath/bin 
+	mkdir -p $fullpath/lib 
+	install -m 557 tetgen $fullpath/bin/ 
+	install -m 557 libtet.a $fullpath/lib/ 
+
 InstallTarget
 diff -Naur old new > makefile.patch
